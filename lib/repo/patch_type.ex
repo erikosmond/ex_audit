9d1ec9e84831f68c219d77c437a0abe83b371fad
Erik Osmond <erikosmond@gmail.com>
2020-10-28 15:46:35 -0400

    add more debugging

--- lib/repo/patch_type.ex
+++ lib/repo/patch_type.ex
@@ -2,7 +2,12 @@ defmodule ExAudit.Type.Patch do
   @behaviour Ecto.Type
 
   def cast(a), do: {:ok, a}
-  def dump(patch), do: {:ok, :erlang.term_to_binary(patch)}
+  # def dump(patch), do: {:ok, :erlang.term_to_binary(patch)}
+  def dump(patch) do
+    IO.inspect(patch, label: "patch passed into dump")
+    IO.inspect(erlang.term_to_binary(patch), label: "patch as binary")
+    {:ok, :erlang.term_to_binary(patch)}
+  end
   def load(binary), do: {:ok, :erlang.binary_to_term(binary)}
   def type, do: :binary
 end

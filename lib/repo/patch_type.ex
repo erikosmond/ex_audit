0cc175099b2ae5670bdd2429d4b0221d9e4d4340
Erik Osmond <erikosmond@gmail.com>
2020-10-29 13:32:41 -0400

    encode64 ex_audit_version patches

--- lib/repo/patch_type.ex
+++ lib/repo/patch_type.ex
@@ -2,16 +2,20 @@ defmodule ExAudit.Type.Patch do
   @behaviour Ecto.Type
 
   def cast(a), do: {:ok, a}
-  # def dump(patch), do: {:ok, :erlang.term_to_binary(patch)}
+
   def dump(patch) do
-    IO.inspect(patch, label: "patch passed into dump")
-    IO.inspect(erlang.term_to_binary(patch), label: "patch as binary")
-    {:ok, :erlang.term_to_binary(patch)}
+    patch
+    |> :erlang.term_to_binary()
+    |> Base.encode64()
+    |> ok()
   end
   def load(binary) do
-    IO.inspect(binary, label: "binary passed into load")
-    IO.inspect(:erlang.binary_to_term(binary), label: "binary to term passed into load")
-    {:ok, :erlang.binary_to_term(binary)}
+    |> :erlang.binary_to_term()
+    |> Base.encode64()
+    |> ok()
   end
+
   def type, do: :binary
+
+  defp ok(res), do: {:ok, res}
 end
